1eef88f36bf549fa8e44ad4a7be29d679bb6abde
Jose Garcia <josecarlosgarciajunior2003@gmail.com>
2024-03-19 11:08:30 -0300

    :ok_hand: Alterações de revisão de código

---
+++ src/app/clients/clients.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Student } from '../student';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrl: './clients.component.css'
+})
+export class ClientsComponent {
+
+  students: Student[] = [];
+  formGroupStudent: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.formGroupStudent = formBuilder.group({
+      id : [''],
+      name : [''],
+      course : ['']
+    });
+  }
+
+  save(){
+  this.students.push(this.formGroupStudent.value);
+  }
+
+}
