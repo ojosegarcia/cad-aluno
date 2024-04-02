a4b8769fe748dd41068f9e4cf4e40cc25cd21abb
Jose Garcia <josecarlosgarciajunior2003@gmail.com>
2024-04-02 11:13:20 -0300

    👌 Implementação da  opção de  alterar e remover

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -20,4 +20,11 @@ save(student:Student): Observable<Student>{
   return this.http.post<Student>(this.url, student);
 }
 
+delete(student:Student): Observable<void> {
+  return this.http.delete<void>(`${this.url}/${student.id}`);
+}
+
+update(student:Student): Observable<Student> {
+  return this.http.put<Student>(`${this.url}/${student.id}`,student);
+}
 }
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -12,6 +12,7 @@ import { StudentService } from '../student.service';
 export class StudentsComponent implements OnInit{
   students: Student[] = [];
   formGroupStudent: FormGroup;
+  isEditing: boolean = false;
 
   ngOnInit(): void {
     this.loadStudents();
@@ -34,9 +35,31 @@ export class StudentsComponent implements OnInit{
   }
 
   save(){
-    this.service.save(this.formGroupStudent.value).subscribe({
-      next: data => this.students.push  (data)
+    if(this.isEditing){
+    this.service.update(this.formGroupStudent.value).subscribe({
+      next: () => {
+        this.loadStudents();
+        this.isEditing= false;
+      }
+    
     });
+  }
+
+  else{
+    this.service.save(this.formGroupStudent.value).subscribe({
+      next: data => this.students.push(data)
+    });
+  }
+  this.formGroupStudent.reset();
+}
+  delete(student:Student){
+    this.service.delete(student) .subscribe({
+      next: () => this.loadStudents()
+    });
+  }
   
+  edit(student:Student){
+    this.formGroupStudent.setValue(student);
+    this.isEditing= true;
   }
 }
